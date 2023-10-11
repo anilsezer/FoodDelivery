9b737716aaa60c1209eccdf80a62e18db908e0f8
anilsezer <anilsezer28@gmail.com>
2023-10-11 15:03:31 +0300

    Project created

---
+++ Food Delivery App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Delivery App
+//
+//  Created by Anıl Sezer on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
